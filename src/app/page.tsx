6e56713e4f8a29e03b2574bd702af9c4b31fe32b
chirantan-2025 <chirantanbramhachari560@gmail.com>
2025-09-22 21:05:43 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import { FormField, FormItem } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex items-center justify-center mt-5'>
+      <Button>Chirantan</Button>
+      <FormItem><Input /></FormItem>
+    </div>
+  )
+}
+
+export default page
