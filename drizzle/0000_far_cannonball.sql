36c53c9a40ae8d41e737ff8afe302416bcb1c85c
ishita-88 <mondalishita618@gmail.com>
2025-11-13 15:57:39 +0530

    .

--- drizzle/0000_far_cannonball.sql
+++ drizzle/0000_far_cannonball.sql
@@ -11,6 +11,6 @@ CREATE TABLE "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"email" varchar(255) NOT NULL,
 	"password" varchar(255) NOT NULL,
-	"role" varchar(50) DEFAULT 'customer',
+	"roles" varchar(50) DEFAULT 'customer',
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
